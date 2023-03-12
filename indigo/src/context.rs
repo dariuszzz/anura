6a848dadec8e54e7ad955ba2aa30e53e3c9cf23d
Dariusz Zielonka <dariuszzielonka727@gmail.com>
2023-03-12 19:49:38 +0100

    split context into indigocontext and rendercontext
    this is so manipulating the ui is not possible in generate_mesh

--- indigo/src/context.rs
+++ indigo/src/context.rs
@@ -17,14 +17,6 @@ where
     V: View<A, R> + 'static,
     R: IndigoRenderer + 'static,
 {
-    pub fn submit_render_commands(&mut self, commands: Vec<R::RenderCommand>) {
-        if let NodeType::Handle(handle) = &self.current {
-            self.app.render_cache.insert(handle.clone(), commands);
-        } else {
-            panic!("Cannot submit commands for a non widget")
-        }
-    }
-
     pub fn issue_rerender(&mut self) {
         if let NodeType::Handle(handle) = &self.current {
 
@@ -44,7 +36,25 @@ where
             panic!("Cannot rerender a non widget")
         }
     }
+}
 
+pub struct RenderContext<'a, 'b, A, V, R>
+where
+    R: IndigoRenderer
+{
+    pub app: &'b mut IndigoApp<'a, A, R>,
+    pub ui_tree: &'b UiTree<A, V, R>,
+    pub current: NodeType,
+}
+
+
+impl<'a, 'b, A, V, R> RenderContext<'a, 'b, A, V, R>
+where
+    A: App<R> + 'static,
+    V: View<A, R> + 'static,
+    R: IndigoRenderer + 'static,
+{
+    
     pub fn render(
         &mut self, 
         handle: &impl AsUntypedHandle, 
@@ -55,27 +65,24 @@ where
         
         if let NodeType::Handle(curr_handle) = &self.current {
             if *curr_handle == handle {
-                panic!("Cannot render self")
+                panic!("Recursive generate_mesh")
             } 
         }
 
         if self.app.render_cache.get(&handle).is_none() {
-            self.ui_tree.run_on_moved_out(&handle, |ui_tree, widget| {
-                let mut ctx = IndigoContext { 
-                    app: self.app,
-                    ui_tree,
-                    current: NodeType::Handle(handle)
-                };
-    
-                widget.handle_event(&mut ctx, view, WidgetEvent::Render { layout })
-            })?;
+            let widget = self.ui_tree.get_untyped_ref(&handle).unwrap();
+            let mut ctx = RenderContext { 
+                app: self.app,
+                ui_tree: self.ui_tree,
+                current: NodeType::Handle(handle)
+            };
+
+            let commands = widget.generate_mesh(&mut ctx, view, layout)?;
+            self.app.render_cache.insert(handle.clone(), commands);
         }
 
         Ok(self.app.render_cache.get(&handle.handle())
             .expect("Render event didnt submit a command vec")
             .clone())
-
-
-
     }
 }
